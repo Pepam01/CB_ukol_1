427f798d04ca876e0834bf7835ccbb80d2507746
pepam <josef.machac@codingbear.com>
2023-02-15 09:26:22 +0100

    initial commit

---
+++ src/main/java/com/example/pipeline_ukol_1/PipelineUkol1Application.java
@@ -0,0 +1,13 @@
+package com.example.pipeline_ukol_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineUkol1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineUkol1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pipeline_ukol_1/PipelineUkol1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipeline_ukol_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineUkol1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
